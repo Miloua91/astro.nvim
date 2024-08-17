eaf4dcc44d26a2a15ed53b7562582508b7e3e1fa
Miloua Mokhtar <miloua23@gmail.com>
2024-08-17 22:36:45 +0100

    fixed conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -7,12 +7,13 @@ return {
         -- Conform will run multiple formatters sequentially
         python = { 'isort', 'black' },
         -- Use a sub-list to run only the first available formatter
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        css = { { 'prettierd', 'prettier' } },
-        astro = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        astro = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
       },
     }
     vim.api.nvim_create_autocmd('BufWritePre', {
